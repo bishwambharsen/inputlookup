b6459911e56fc4d67350faa2fd4a9341cad1cf58
Bishwambhar Sen <bishwambhar.sen@gmail.com>
2019-02-26 13:36:41 +0800

    Updated Apex controller logic and added action button

--- force-app/main/default/aura/CloneQuoteToOpportunity/CloneQuoteToOpportunityHelper.js
+++ force-app/main/default/aura/CloneQuoteToOpportunity/CloneQuoteToOpportunityHelper.js
@@ -2,20 +2,30 @@
     cloneQuote: function(cmp, event) {
         var currentQuoteId = cmp.get('v.recordId');
         var cloneToOpptyId = cmp.get('v.opportunityId');
-
+        
         var action = cmp.get('c.cloneQuoteToNewOppty');
-
+        
         action.setCallback(this, function(response) {
             var state = response.getState();
-
+            
             if (state === "SUCCESS") {
                 console.log("Quote successfully cloned to new opportunity");
+                showToast("Success","The quote has been sucessfully cloned.");
             } else if (state === "ERROR") {
                 var error = response.getError();
                 console.log("Error encountered: " + JSON.stringify(error));
             }
         });
-
+        
         $A.enqueueAction(action);
+    },
+    
+    showToast : function(title, message) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            title:title ,
+            message:message 
+        });
+        toastEvent.fire();
     }
 })

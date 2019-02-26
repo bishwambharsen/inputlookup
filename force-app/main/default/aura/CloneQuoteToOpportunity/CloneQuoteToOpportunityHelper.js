74e2446998bb9e3f916a2dfc9e0f390e14cf9c34
Bishwambhar Sen <bishwambhar.sen@gmail.com>
2019-02-26 14:10:46 +0800

    lightning spinner and showToast messages

--- force-app/main/default/aura/CloneQuoteToOpportunity/CloneQuoteToOpportunityHelper.js
+++ force-app/main/default/aura/CloneQuoteToOpportunity/CloneQuoteToOpportunityHelper.js
@@ -4,25 +4,39 @@
         var cloneToOpptyId = cmp.get('v.opportunityId');
         
         var action = cmp.get('c.cloneQuoteToNewOppty');
-        
+        action.setParams({
+            quoteId: currentQuoteId,
+            opptyId: cloneToOpptyId
+        })
+        toggleSpinner();
         action.setCallback(this, function(response) {
             var state = response.getState();
-            
+            toggleSpinner();
             if (state === "SUCCESS") {
                 console.log("Quote successfully cloned to new opportunity");
-                showToast("Success","The quote has been sucessfully cloned.");
+                $A.get('e.force:closeQuickAction').fire();
+                showToast("success","Success","The quote has been sucessfully cloned.");
+                
             } else if (state === "ERROR") {
                 var error = response.getError();
                 console.log("Error encountered: " + JSON.stringify(error));
+                $A.get('e.force:closeQuickAction').fire();
+                showToast('error',"Error","Something went wrong!");
+                
             }
         });
         
         $A.enqueueAction(action);
     },
     
-    showToast : function(title, message) {
+    toggleSpinner: function(cmp, event){
+        $A.util.toggleClass(cmp.find('spinner'),'slds-hide');
+    },
+    
+    showToast : function(type,title, message) {
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
+            type:type,
             title:title ,
             message:message 
         });
